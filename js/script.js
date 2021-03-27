94633befcfbf717ffb28a7bfdefe431868704154
Yulia <tarasenkoylia2716@gmail.com>
2021-03-27 21:56:42 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+console.log('aaa' + " - AAA");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 *2 !==   6);
+
+const i = true,
+    a = true;
+    
+console.log(!i || !a);
